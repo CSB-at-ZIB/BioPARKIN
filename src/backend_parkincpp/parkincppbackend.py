8fe833f80339acb5bf32dd6418f9fc4782c35c22
Moritz Wade <moritzwade@gmail.com>
2011-10-14 10:08:39 +0200

    Removed the explicit "del"s from backend's __del__()
    closes #38

--- src/backend_parkincpp/parkincppbackend.py
+++ src/backend_parkincpp/parkincppbackend.py
@@ -108,21 +108,6 @@ class ParkinCppBackend(BaseBackend):
 
     def __del__(self):
 #        self.exiting = True
-
-        # explicitely destroy SWIG-wrapped objects
-        del self.bioSystem
-        del self.bioSystem
-        del self.sensitivityTimepointsVector
-        del self.timepointVector
-        del self.measurementMapVector
-        del self.breakpoints
-        del self.eventMap
-        del self.paramMap
-        del self.paramThresholdMap
-        del self.speciesThresholdMap
-        del self.iOpt
-        del self.sensitivityTrajectoryMap
-
         self.wait() #blocks this thread until it has finished
 
     def setMode(self, mode):
